8bb662c0b9d1156c80de7a7080ba5b9324153fef
Nabuduwa-ketty <nabuduwaketty@gmail.com>
2019-06-13 20:08:51 +0300

    loops work added

--- billing.js
+++ billing.js
@@ -1,6 +1,6 @@
 /*This is the percentage that determines the transaction charge*/
 const transactionCharge = 0.025;
 var amountTendered = 100000;
-BilledAmount = amountTendered-(amountTendered*transactionCharge)
+BilledAmount = amountTendered-(amountTendered*transactionCharge);
 console.log("This is what the billed amount is (less transaction charges)");
 console.log(BilledAmount);
---
+++ null_checkers.js
@@ -0,0 +1,21 @@
+
+var i;
+for(i=1; i<=10; i++)
+	{
+		
+		let number = prompt('Please input a number?','');
+	 if (number>0) 
+	 
+	 	{
+	 	console.log(1);
+	 	}
+	   else if (number<0)
+	   {
+	   	console.log(-1);
+	   } 
+	
+         else 
+         {
+          console.log(0);
+      }
+     }
